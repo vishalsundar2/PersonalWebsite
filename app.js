164e8486a8fbbf87271698acaf48c050da76b2b5
vishalsundar2 <114627628+vishalsundar2@users.noreply.github.com>
2023-08-11 23:31:42 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,67 @@ scrollUp.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+  });
+
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+function addClassWhenVisible(element, className) {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+
+    if (elementTop - windowHeight <= 0) {
+      element.classList.add(className);
+    }
+  }
+
+  // Call the function on scroll
+  window.addEventListener("scroll", function () {
+    const moreAbout = document.querySelector(".more-about");
+    const skills = document.querySelector(".skills");
+    const projects = document.querySelector(".projects");
+    const contact = document.querySelector(".contact");
+
+    addClassWhenVisible(moreAbout, "fade-in");
+    addClassWhenVisible(skills, "fade-in");
+    addClassWhenVisible(projects, "fade-in");
+    addClassWhenVisible(contact, "fade-in");
+  });
+
+  // Function to handle fading in sections when they enter the viewport
+  function fadeSections() {
+    const sections = document.querySelectorAll('.fade-in');
+    
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+      
+      if (sectionTop < windowHeight) {
+        // Section is in the viewport
+        section.style.opacity = '1';
+        section.style.transform = 'translateY(0)';
+      }
+    });
+  }
+
+  // Attach the function to the scroll event
+  window.addEventListener('scroll', fadeSections);
+
+  // Initial call to fadeSections to apply the effect on page load
+  fadeSections();
